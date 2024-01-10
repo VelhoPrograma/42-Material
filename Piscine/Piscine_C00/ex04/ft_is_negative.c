1d2d2adce0771a46d9eb44b4b324106b9b6be52c
VelhoPrograma <155855218+VelhoPrograma@users.noreply.github.com>
2024-01-10 22:16:25 +0000

    Update ft_is_negative.c

--- Piscine/Piscine_C00/ex04/ft_is_negative.c
+++ Piscine/Piscine_C00/ex04/ft_is_negative.c
@@ -2,16 +2,20 @@
 
 void	ft_is_negative(int n)
 {
-	char	c;
-
 	if (n < 0)
 	{
-		c = 'N';
-		write(1, &c, 1);
+		write(1, "N", 1);
 	}
 	else
 	{
-		c = 'P';
-		write(1, &c, 1);
+		write(1, "P", 1);
 	}
 }
+
+/*int main(void)
+{
+	ft_is_negative(1);
+	ft_is_negative(0);
+	ft_is_negative(-1);
+	return (0);
+}*/
